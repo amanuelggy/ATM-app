7706c5f07044047849666532f07ed45820d5d1e1
Amanuel <amanual2@gmail.com>
2020-12-19 16:07:32 -0500

    ATM App started project

---
+++ src/test/java/com/atmapp/com/AtmAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atmapp.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtmAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
